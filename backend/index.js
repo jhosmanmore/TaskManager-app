faaf5200f5e1556053f752393c9663f7675b2f20
Jhosman Moreno <jhosmanmoreno@gmail.com>
2025-06-07 21:27:54 -0500

    v3

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,8 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('MongoDB Atlas conectado'))
 .catch((err) => console.error(err));
 
